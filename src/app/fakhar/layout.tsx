69fae1e6a71c03af5c265bcd945a582ed888425a
Muhammad Fakhar Khan <khhanfakhar@gmail.com>
2025-09-13 22:19:44 +0500

    init

---
+++ src/app/fakhar/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+<>
+        <h2>Nav Bar</h2>
+        {/* If we want to use Navbar on every page its better to use the main layout because the main layout is visible to every page */}
+</>
+
+  );
+}
---
+++ src/app/fakhar/page.tsx
@@ -0,0 +1,5 @@
+export default function Fakhar() {
+  return (
+      <h1 className="text-2xl text-center">Fakhar Khan</h1>
+  );
+}
